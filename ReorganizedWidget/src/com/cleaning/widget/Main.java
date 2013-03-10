af0a3c76dab31ae406ff8924336932a0d7ea4ec0
alexkurocha <alexaleku@gmail.com>
2013-03-10 15:33:09 -0700

    345

--- ReorganizedWidget/src/com/cleaning/widget/Main.java
+++ ReorganizedWidget/src/com/cleaning/widget/Main.java
@@ -18,7 +18,7 @@ public class Main extends AppWidgetProvider {
 	for(int i=0; i<appWidgetIds.length; i++) {
 	int widgetID = appWidgetIds[i];
 		
-//	jkyfvkivljj,hvjhv	int appWidgetId = 0;
+//	77777	int appWidgetId = 0;
 		
 	
 	
